ed1721bb3b23cbf351334679a440a947f09926bf
Dell Pc <mudasir9757@gmail.com>
2023-04-17 19:54:22 +0500

    add routes with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  root 'home#index'
+  resources :users, only: [:index, :show] do
+    resources :foods, only: [:index, :show, :new, :create, :destroy] do
+      resources :recipefoods, only: [:create]
+    end
+    resources :recipes, only: [:create, :new, :destroy, :index]do
+      resources :recipefoods, only: [:create]
+    end
+  end
 end
