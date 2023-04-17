7bf26e2a4018fe2a8c7d669af43721a9daa467d1
Dell Pc <mudasir9757@gmail.com>
2023-04-17 19:58:30 +0500

    Add seeds and update schema with migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+   third_user = User.create!(email: 'mudasir@gmail.com',  password:'123456', name: 'mudasir')
+   four_user = User.create!(email: 'yuvenal@gmail.com',  password:'123456', name: 'yuvenal')
+   
+Recipe.create!(user_id: third_user, name: 'Recipe by Mudasir 1', description: 'This is Tom first post This is Tom first postThis is Tom first postThis is Tom first postThis is Tom first postThis is Tom first postThis is Tom first post' )
+Recipe.create!(user_id: third_user, name: 'Recipe by Mudasir 2', description: 'This is Tom second post This is Tom second postThis is Tom second postThis is Tom second postThis is Tom second postThis is Tom second postThis is Tom second postThis is Tom second post')
+Food.create!(user_id: four_user, name: 'Recipe by Yuvenal 1', description: 'This is Lilly first postThis is Lilly first postThis is Lilly first postThis is Lilly first postThis is Lilly first postThis is Lilly first post')
+Food.create!(user_id: four_user, name: 'Recipe by Yuvenal 2', description: 'This is Lilly first postThis is Lilly first postThis is Lilly first postThis is Lilly first postThis is Lilly first postThis is Lilly first post')
