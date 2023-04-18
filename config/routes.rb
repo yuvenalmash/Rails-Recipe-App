5cdfd67246bda6e28483cf8dd6c88ccba2f79338
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-18 14:25:19 +0300

    Add foods_path route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
-  resources :users, only: [:index, :show] do
-    resources :foods, only: [:index, :show, :new, :create, :destroy] do
+  root "home#index"
+  resources :foods, only: %i[index show new create destroy]
+  resources :users, only: %i[index show] do
+    resources :foods, only: %i[index show new create destroy] do
       resources :recipefoods, only: [:create]
     end
-    resources :recipes, only: [:create, :new, :destroy, :index]do
+    resources :recipes, only: %i[create new destroy index] do
       resources :recipefoods, only: [:create]
     end
   end
