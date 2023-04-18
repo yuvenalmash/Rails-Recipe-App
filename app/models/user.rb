dbfbd47eacc8dfab5df506d6de9890b89848f87f
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-18 14:21:34 +0300

    Fix user moodel associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-  has_many :foods, foreign_key: 'users_id'
-  has_many :recipes, foreign_key: 'users_id'
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :confirmable
+  has_many :foods, foreign_key: 'user_id'
+  has_many :recipes, foreign_key: 'user_id'
   validates :name, presence: true
 end
