7660a601dc0a60a943e81a8702ce18c2f350510b
Dell Pc <mudasir9757@gmail.com>
2023-04-18 17:30:19 +0500

    Fix rubocop linters

--- app/controllers/publicrecipes_controller.rb
+++ app/controllers/publicrecipes_controller.rb
@@ -1,6 +1,5 @@
 class PublicrecipesController < ApplicationController
-    def index
-      @recipes = Recipe.where(public: true).order(created_at: :desc)
-    end
+  def index
+    @recipes = Recipe.where(public: true).order(created_at: :desc)
   end
-  
+end
