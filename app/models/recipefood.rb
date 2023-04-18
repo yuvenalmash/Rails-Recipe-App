75293d72ddf9a7f817034aeb213e2a7f50686936
Dell Pc <mudasir9757@gmail.com>
2023-04-18 11:32:33 +0500

    Update recipefood model

--- app/models/recipefood.rb
+++ app/models/recipefood.rb
@@ -1,5 +1,6 @@
 class Recipefood < ApplicationRecord
-  belongs_to :recipe
   belongs_to :food, class_name: 'Food', foreign_key: 'food_id'
   belongs_to :recipe, class_name: 'Recipe', foreign_key: 'recipe_id'
+  
+  validates :quantity, presence: true
 end
