f58026a2bdd7e2fe6d67d51abe4aa760c2e314fc
Dell Pc <mudasir9757@gmail.com>
2023-04-20 03:13:16 +0500

    Add navigation menu

--- app/controllers/publicrecipes_controller.rb
+++ app/controllers/publicrecipes_controller.rb
@@ -1,5 +1,6 @@
 class PublicrecipesController < ApplicationController
   def index
+    @user = User.find(current_user.id)
     @recipes = Recipe.where(public: true).order(created_at: :desc)
   end
 end
