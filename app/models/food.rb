47233a4b6e96af1ed9a0e2730e8432313dea7e76
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-19 16:32:06 +0300

    Add validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,9 @@
 class Food < ApplicationRecord
   has_many :recipefoods, foreign_key: 'food_id'
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
+  validates :measurement_unit, presence: true
 end
