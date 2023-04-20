c38b8089882f3c856fbbd603868cf7feb01fd8cc
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-20 16:11:43 +0300

    fix linter errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
-  has_many :recipefoods, foreign_key: "food_id"
-  belongs_to :user, class_name: "User", foreign_key: "user_id"
+  has_many :recipefoods, foreign_key: 'food_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   before_destroy :delete_recipe_foods
 
   validates :name, presence: true
@@ -11,6 +11,6 @@ class Food < ApplicationRecord
   private
 
   def delete_recipe_foods
-    self.recipefoods.destroy_all
+    recipefoods.destroy_all
   end
 end
