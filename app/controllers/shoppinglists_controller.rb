2cef1c45687092f2740bc5ff18e6728496249cdc
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-21 04:07:21 +0300

    fix linter errors

--- app/controllers/shoppinglists_controller.rb
+++ app/controllers/shoppinglists_controller.rb
@@ -2,8 +2,7 @@ class ShoppinglistsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    user = current_user
-    recipes = user.recipes.includes(recipefoods: :food)
+    recipes = current_user.recipes.includes(recipefoods: :food)
     shoppinglist_raw = {}
 
     recipes.each do |recipe|
@@ -20,20 +19,23 @@ class ShoppinglistsController < ApplicationController
       end
 
       @shoppinglist = []
-      shoppinglist_raw.values.each do |recipefood|
-        quantity = recipefood[:quantity] - recipefood[:food].quantity
-        if quantity > 0
-          @shoppinglist << {
-            food: recipefood[:food],
-            quantity: quantity,
-            price: recipefood[:food].price * quantity
-          }
-        end
-      end
-
-      @shoppinglist_count = @shoppinglist.count
-      @shoppinglist_price = @shoppinglist.sum { |item| item[:price] }
+      update_shoppinglist(shoppinglist_raw)
     end
-    @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
+  private
+
+  def update_shoppinglist(shoppinglist_raw)
+    shoppinglist_raw.values.each do |recipefood|
+      quantity = recipefood[:quantity] - recipefood[:food].quantity
+      next unless quantity.positive?
+
+      @shoppinglist << {
+        food: recipefood[:food],
+        quantity: quantity,
+        price: recipefood[:food].price * quantity
+      }
+    end
   end
 end
